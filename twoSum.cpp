ca9fbdf4a3a574809e48c4e96e013590bad172e1
Usmaan Sayed <161072079+usmaan677@users.noreply.github.com>
2025-09-14 14:01:38 -0500

    two sum

---
+++ twoSum.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int,int> seen;
+    
+            for(int i=0;i<nums.size();i++){
+                int need = target-nums[i];
+    
+                if(seen.count(need)){
+                    return {seen[need], i};
+                }
+                seen[nums[i]] = i;
+            }
+            return{};
+        }
+    };
